7fbca2ce8fb6a059f17f17ec644c2163247c29aa
Swapnil <swapnil.patil@silicus.com>
2014-03-07 14:37:30 +0530

    Added Validations to the employee model

---
+++ BenchRockers/BenchRockers/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BenchRockers.Models
+{
+    public class Employee
+    {
+        public Employee() { }
+        public int EmployeeId { get; set; }
+        
+        public string Name { get; set; }
+        public int RoleId { get; set; }
+        public virtual Role Roles { get; set; }
+        
+        public string Account { get; set; }
+        
+        public float TotalExp { get; set; }
+        
+        public string Location { get; set; }
+        public bool IsOnBench { get; set; }
+    }
+}
